9ed8034f0d71d4d351597f6322a564c49ed7f19b
C01-JNU <16254621+C01-JNU@user.noreply.gitee.com>
2026-04-06 01:44:18 +0000

    引入MNN准备开搞

--- include/preprocess/preprocess.h
+++ include/preprocess/preprocess.h
@@ -1,3 +1,22 @@
+// Copyright (C) 2026 C01-JNU
+// SPDX-License-Identifier: GPL-3.0-only
+//
+// This file is part of FishTotem.
+//
+// FishTotem is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// FishTotem is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with FishTotem. If not, see <https://www.gnu.org/licenses/>.
+
+
 // preprocess.h
 // 前处理主类：去噪（CPU）+ Census 变换（可选 GPU），运行时选择后端
 // 最后更新: 2026-04-05
